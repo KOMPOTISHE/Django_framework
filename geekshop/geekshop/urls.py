075db5de152dd28203fa8a7e2a85a4bd7b9c40da
KOMPOTISHE <danilvankov@yandex.ru>
2022-01-24 11:09:21 +0700

    update Hw

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth')),
     # path('admin/', admin.site.urls),
     path('basket/', include('basketapp.urls', namespace='basket')),
-    path('admin/', include('adminapp.urls', namespace='admin'))
+    path('admin/', include('adminapp.urls', namespace='admin')),
+    path('', include('social_django.urls', namespace='social')),
+    path('order/', include('ordersapp.urls', namespace='order'))
 
 ]
 
